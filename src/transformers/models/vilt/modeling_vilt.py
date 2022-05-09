76250c1ee0c8a28b833520694d30894cb7158453
Tejas Srinivasan <tejas.srinivasan95@gmail.com>
2022-05-09 12:22:17 -0700

    Removed intermediate hidden activation

--- src/transformers/models/vilt/modeling_vilt.py
+++ src/transformers/models/vilt/modeling_vilt.py
@@ -452,15 +452,15 @@ class ViltIntermediate(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.dense = nn.Linear(config.hidden_size, config.intermediate_size)
-        if isinstance(config.hidden_act, str):
-            self.intermediate_act_fn = ACT2FN[config.hidden_act]
-        else:
-            self.intermediate_act_fn = config.hidden_act
+        #if isinstance(config.hidden_act, str):
+        #    self.intermediate_act_fn = ACT2FN[config.hidden_act]
+        #else:
+        #    self.intermediate_act_fn = config.hidden_act
 
     def forward(self, hidden_states):
 
         hidden_states = self.dense(hidden_states)
-        hidden_states = self.intermediate_act_fn(hidden_states)
+        #hidden_states = self.intermediate_act_fn(hidden_states)
 
         return hidden_states
 
